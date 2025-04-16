b4bc37f9656df39f632eda79ca9ee68ea01fd195
Yusuf AKIN <akinyusuf17@gmail.com>
2025-04-16 09:19:33 +0300

    feat: implement a sample endpoint for reference (with a test case)

---
+++ backend/README.md
@@ -0,0 +1,17 @@
+# Quick Setup
+
+## Development Server
+Run the development server:
+```bash
+./manage.py runserver 9000
+```
+
+## Tests
+```bash
+./manage.py test
+```
+
+
+
+
+
--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,10 @@
+from typing import cast
+from django.http import HttpResponse
 from django.test import TestCase
 
-# Create your tests here.
+class GetTimeTest(TestCase):
+    def test_get_time_success(self):
+        response = cast(HttpResponse, self.client.get('/api/time?name=Arda'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('time', response.json()) # type: ignore
+        self.assertEqual(response.json()['name'], 'Arda') # type:ignore
