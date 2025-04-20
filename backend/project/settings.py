d88558cf9f30047d307d1efb4540b3de246d32ca
Berkay Bilen <berkay.bilen@boun.edu.tr>
2025-04-21 00:51:12 +0300

    MySQL integration with temp credentials using docker

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mydb",
+        "USER": "django",
+        "PASSWORD": "djangopass",
+        "HOST": "localhost",  # or 'db' if Django is also running in Docker
+        "PORT": "3306",
     }
 }
 
