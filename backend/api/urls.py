55378e3dde9834a8639003ff864ff97a7dbab4c0
nuriba <114422166+nuriba@users.noreply.github.com>
2025-04-22 11:36:00 +0300

    115 coding part for backend api introduction (#118) (#144)
    * Create a basic user model with serializers for validations and type checks, implement endpoints with service and repository files to create and list users.
    
    * Add imports for settings.py and views
    
    * black format
    
    * Add tests for user scenario, suggested a command in readme file o run tests.
    
    * Validation and error case in serializers
    
    Co-authored-by: Berkay Bilen <77581793+berkaybilen@users.noreply.github.com>

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,12 @@
 # api/urls.py - Update your existing file
 from django.urls import path
-from .views import get_time, login_view
+
+from .views import get_time, get_users, create_user_view, login_view
 
 urlpatterns = [
     path("time", get_time, name="get_time"),
     path("login", login_view, name="login"),
+    path("users/", get_users),
+    path("users/create/", create_user_view),
 ]
+
