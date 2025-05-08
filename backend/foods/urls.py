749a00f12d7b7061fda187c5aa61a7a82ef107e8
ArdaSaygan <ardasagii77@gmail.com>
2025-05-08 23:08:26 +0300

    feat: added endpoint to retrieve first 10 food items in the database

--- backend/foods/urls.py
+++ backend/foods/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import FoodCatalog
 
-urlpatterns = []
+urlpatterns = [
+    path("get_foods", FoodCatalog.as_view(), name="get_foods"),
+]
--- backend/foods/views.py
+++ backend/foods/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from foods.models import FoodEntry
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from foods.serializers import FoodEntrySerializer
 
-# Create your views here.
+
+class FoodCatalog(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        """
+        GET /foods
+        Fetch and return a list of the first 10 food entries in the system.
+        """
+        objects = FoodEntry.objects.all()[:10]
+        serializer = FoodEntrySerializer(objects, many=True)
+        return Response(serializer.data)
