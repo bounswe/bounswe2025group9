62df7aa69144a72b4c3ba2d68eb2047ec65854a0
Yusuf AKIN <akinyusuf17@gmail.com>
2025-05-08 21:42:45 +0300

    ref: remove `health_check` endpoint that was used for testing

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, health_check
+from .views import PostViewSet
 
 router = DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
 
 urlpatterns = [
-    path("", health_check),
     path("", include(router.urls)),
 ]
--- backend/forum/views.py
+++ backend/forum/views.py
@@ -1,18 +1,7 @@
 from rest_framework import viewsets, permissions
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework.request import Request
 
 from .models import Post
 from .serializers import PostSerializer
-
-
-@api_view(["GET"])
-@permission_classes([AllowAny])
-def health_check(request: Request):
-    print(request)
-    return Response({"status": "forum app alive"})
 
 
 class PostViewSet(viewsets.ModelViewSet):
