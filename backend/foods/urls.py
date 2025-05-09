a62bb2ca320c3c4e20a45e7e07b322cd9989bbc2
ArdaSaygan <ardasagii77@gmail.com>
2025-05-09 06:23:38 +0300

    fix: change route name to be more general

--- backend/foods/urls.py
+++ backend/foods/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FoodCatalog
 
 urlpatterns = [
-    path("get_foods", FoodCatalog.as_view(), name="get_foods"),
+    path("", FoodCatalog.as_view(), name="get_foods"),
 ]
