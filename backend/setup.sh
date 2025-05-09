71b67c4a3de6a3df5f3f7970ecf73443b0b87764
Yusuf AKIN <71184281+orbitstabilizer@users.noreply.github.com>
2025-05-09 10:38:13 +0300

    Bootstrap forum app (#204)
    * auto: init forum app
    
    * ... : integrate the forum app with the project && test w/ dummy endpoint
    
    * feat: create `Post` model and serializers
    
    * feat: create crud view for `Post` model && add test
    
    * chore: update postman_collection with `Post` crud endpoints
    
    * chore: migrate `Post` model
    
    * doc: update readme with DB setup instructions
    
    * ref: remove `health_check` endpoint that was used for testing

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,3 +1,4 @@
 export BACKEND_SECRET_KEY="super-secret-key"
-export MYSQL_USER = "django"
-export MYSQL_PASSWORD = "djangopass"
+export MYSQL_USER="django"
+export MYSQL_PASSWORD="djangopass"
+export MYSQL_HOST="localhost"
