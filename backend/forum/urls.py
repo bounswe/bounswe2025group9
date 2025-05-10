ef59004ad18794af3864c569b0f53af9a207c624
Yusuf AKIN <akinyusuf17@gmail.com>
2025-05-11 00:54:10 +0300

    feat: add crud view to `Comment` model && add owner restrictions

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, TagViewSet
+from .views import CommentViewSet, PostViewSet, TagViewSet
 
 router = DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
 router.register("tags", TagViewSet, basename="tag")
-
+router.register("comments", CommentViewSet, basename="comment")
 urlpatterns = [
     path("", include(router.urls)),
 ]
--- backend/forum/views.py
+++ backend/forum/views.py
@@ -1,24 +1,55 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, permissions, mixins
+from rest_framework import viewsets, permissions, mixins, generics
 from rest_framework.filters import OrderingFilter
 
-from .models import Post, Tag
-from .serializers import PostSerializer, TagSerializer
+from .models import Post, Tag, Comment
+from .serializers import PostSerializer, TagSerializer, CommentSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Allow read-only access to everyone, but only allow object owner to edit or delete.
+    """
+
+    def has_object_permission(self, request, _, obj) -> bool:  # type:ignore
+        # SAFE_METHODS = GET, HEAD, OPTIONS
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return hasattr(obj, "author") and obj.author == request.user
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ["tags"]
     ordering_fields = ["created_at"]
+
+    def get_queryset(self):
+        return Post.objects.all().order_by("-created_at")
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
 class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    queryset = Tag.objects.all().order_by("name")
     serializer_class = TagSerializer
-    permission_classes = []
+    permission_classes = []  # authentication is not a big deal for this
+
+    def get_queryset(self):
+        return Tag.objects.all().order_by("name")
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Comment.objects.all().order_by("created_at")
+        post_id = self.request.query_params.get("post")
+        if post_id is not None:
+            queryset = queryset.filter(post_id=post_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
