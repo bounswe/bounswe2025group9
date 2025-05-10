51a7a822f96da3ffd05d292e10e6e9f600f25c74
Berkay Bilen <berkay.bilen@boun.edu.tr>
2025-05-10 18:09:03 +0300

    add sqlite for tests db

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -85,18 +85,26 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "USER": os.getenv("MYSQL_USER", "django"),
-        "PASSWORD": os.getenv("MYSQL_PASSWORD", "djangopass"),
-        "NAME": os.getenv("MYSQL_DATABASE", "mydb"),
-        "HOST": os.getenv(
-            "MYSQL_HOST", "db"
-        ),  # changed from mysql-db to db to match docker-compose service name
-        "PORT": os.getenv("MYSQL_PORT", "3306"),
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
     }
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "USER": os.getenv("MYSQL_USER", "django"),
+            "PASSWORD": os.getenv("MYSQL_PASSWORD", "djangopass"),
+            "NAME": os.getenv("MYSQL_DATABASE", "mydb"),
+            "HOST": os.getenv(
+                "MYSQL_HOST", "db"
+            ),  # changed from mysql-db to db to match docker-compose service name
+            "PORT": os.getenv("MYSQL_PORT", "3306"),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
