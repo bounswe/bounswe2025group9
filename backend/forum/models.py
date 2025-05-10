7727e7992c67c0ad6c22bfa1097d2eb97f1b5343
Yusuf AKIN <akinyusuf17@gmail.com>
2025-05-10 23:42:09 +0300

    feat: create `Tag` model

--- backend/forum/models.py
+++ backend/forum/models.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.conf import settings
 
 
+class Tag(models.Model):
+    objects: Any
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
 class Post(models.Model):
     objects: Any
     title = models.CharField(max_length=200)
@@ -10,6 +18,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
