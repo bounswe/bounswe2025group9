03eb025618a8516ea8f56b9ccded3fb522d0ac3b
Yusuf AKIN <akinyusuf17@gmail.com>
2025-05-10 23:45:49 +0300

    feat: integrate sort by created_at and filter by tags with `Post` view
    - introduced a new dependency: `django-filter`
    - define default pagination settings and filter backend

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import PostViewSet, TagViewSet
 
 router = DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
+router.register("tags", TagViewSet, basename="tag")
 
 urlpatterns = [
     path("", include(router.urls)),
