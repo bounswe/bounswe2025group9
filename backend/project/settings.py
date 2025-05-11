d5e7f0aa4bc91dedbc5a3bd7197b7829d5ea0c0a
Berkay Bilen <77581793+berkaybilen@users.noreply.github.com>
2025-05-11 11:09:02 +0300

    129 change password functionality (#177)
    * add endpoint for change password, add tests, seperate test files into folders
    
    ---------
    
    Co-authored-by: Furkan Bilsel <13202351+ffbilsel@users.noreply.github.com>

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta  # import this library top of the settings.py file
 from pathlib import Path
-import os
+import os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,19 +85,26 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "USER": os.getenv("MYSQL_USER", "django"),
-        "PASSWORD": os.getenv("MYSQL_PASSWORD", "djangopass"),
-        "NAME": os.getenv("MYSQL_DATABASE", "mydb"),
-        "HOST": os.getenv(
-            "MYSQL_HOST", "db"
-        ),  # changed from mysql-db to db to match docker-compose service name
-        "PORT": os.getenv("MYSQL_PORT", "3306"),
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "USER": os.getenv("MYSQL_USER", "django"),
+            "PASSWORD": os.getenv("MYSQL_PASSWORD", "djangopass"),
+            "NAME": os.getenv("MYSQL_DATABASE", "mydb"),
+            "HOST": os.getenv(
+                "MYSQL_HOST", "db"
+            ),  # changed from mysql-db to db to match docker-compose service name
+            "PORT": os.getenv("MYSQL_PORT", "3306"),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
