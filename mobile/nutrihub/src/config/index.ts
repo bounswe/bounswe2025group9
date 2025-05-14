505dc4a934389242652f582046f37dfd9f59be53
yusuf anil yazici <56488393+y4z1c1@users.noreply.github.com>
2025-05-14 14:00:31 +0300

    fix: remove depreceated packages that are preventing build (#296)
    Co-authored-by: nft entreprenaur <56488393+yay61@users.noreply.github.com>

--- mobile/nutrihub/src/config/index.ts
+++ mobile/nutrihub/src/config/index.ts
@@ -1,7 +1,7 @@
 // src/config/index.ts
 export const API_CONFIG = {
   BASE_URL: __DEV__ 
-    ? 'http://164.92.234.228/api'  // Development, ADD PUBLIC IP HERE!
-    : 'https://api.nutrihub.com',  // Production
+    ? 'http://164.92.234.228/api'  // development url without https
+    : 'https://nutrihub.fit/api',  // production
   TIMEOUT: 10000,
 };
