1ca988ead92a47e6001ec9cced61d129d7b79dba
Yusuf AKIN <71184281+orbitstabilizer@users.noreply.github.com>
2025-05-14 22:44:50 +0300

    Consume random image api from Foodish (#322)
    feat: consume random image api from Foodish

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import TimeView, WikidataEntityView
+from .views import TimeView, WikidataEntityView, random_food_image
 
 urlpatterns = [
     path("time", TimeView.as_view(), name="get-time"),
     path(
-        "food/<str:entity_id>", WikidataEntityView.as_view(), name="get-wikidata-entity"
+        "wiki-data/<str:entity_id>",
+        WikidataEntityView.as_view(),
+        name="get-wikidata-entity",
     ),
+    path("random-food-image/", random_food_image, name="random_food_image"),
 ]
