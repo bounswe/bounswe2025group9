dd71678bd69abb5e82bd0efeeff3e0301b4c296d
ArdaSaygan <ardasagii77@gmail.com>
2025-10-16 22:29:51 +0300

    feat: the basis for Create Meal Plan page

---
+++ frontend/src/pages/mealplanner/MealPlanner.tsx
@@ -0,0 +1,14 @@
+
+
+const MealPlanner = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold mb-4">Meal Planner</h1>
+            <p>Welcome to the Meal Planner! Here you can create and manage your meal plans.</p>
+            {/* Add more meal planner functionalities here */}
+        </div>
+    )
+}
+
+export default MealPlanner
+
