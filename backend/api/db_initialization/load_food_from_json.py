9f772b49d71fe95ce6dad9701e987ddf00d3fbbf
ArdaSaygan <ardasagii77@gmail.com>
2025-11-23 03:34:21 +0300

    fix: failed tests due to import

--- backend/api/db_initialization/load_food_from_json.py
+++ backend/api/db_initialization/load_food_from_json.py
@@ -13,7 +13,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 django.setup()
 
 from foods.models import FoodEntry, Allergen
-from nutrition_score import calculate_nutrition_score
+from api.db_initialization.nutrition_score import calculate_nutrition_score
 
 
 class FoodLoader:
